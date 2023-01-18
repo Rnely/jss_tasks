38420f9f696dd772bac3773c14ee2870cda6e9b2
ricards <ricards.sinkjavics@accenture.com>
2023-01-18 13:35:45 +0200

    task2 complete

---
+++ task2/script.js
@@ -0,0 +1,5 @@
+function getFormvalue() {
+    const fname = document.querySelector('[name="fname"]').value;
+    const lname = document.querySelector('[name="lname"]').value;
+    console.log(fname, lname);
+}
